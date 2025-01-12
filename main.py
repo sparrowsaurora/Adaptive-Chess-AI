ca5af1492db05b9eafdcc9ed2c498b00a54327fa
Sparrows <35826584+sparrowsaurora@users.noreply.github.com>
2025-01-12 21:32:45 +0800

    implemented class "ChessGame"

--- main.py
+++ main.py
@@ -1,37 +1,43 @@
 import chess
 
-def legal_moves(board):
-    move_list = []
-    for legal_move in board.legal_moves:
-        legal_move = str(legal_move)
-        move_list.append(legal_move)  # Print each legal move
+class ChessGame:
 
-    for move in move_list:
-        print(str(move), end=", ")
-    print("")
+    def main(self):
+        board = chess.Board()
+        print(board)
 
+        while not board.is_game_over():
+            print("Legal moves:")
+            self.legal_moves(board)
+            move = input("Your move: ")
+            if chess.Move.from_uci(move) in board.legal_moves:
+                try:
+                    board.push(chess.Move.from_uci(move))  # Add the move to the board.
+                except:
+                    board.pop()
+                    print(f"{move} is not valid")
+                print(board)
+            else:
+                print("Invalid move, try again!")
 
-
-
-def main():
-    board = chess.Board()
-    print(board)
-
-    while not board.is_game_over():
-        print("Legal moves:")
-        legal_moves(board)
-        move = input("Your move: ")
-        if chess.Move.from_uci(move) in board.legal_moves:
-            try:
-                board.push(chess.Move.from_uci(move))  # Add the move to the board.
-            except:
-                board.pop()
-                print(f"{move} is not valid")
-            print(board)
-        else:
-            print("Invalid move, try again!")
-
-        if board.is_game_over():
-            break
+            if board.is_game_over():
+                break
     
-main()
+    def legal_moves(self, board):
+        '''
+            prints each legal move for every turn
+
+            prints the move in the UCI format: 'e2e4'
+        '''
+        move_list = []
+        for legal_move in board.legal_moves:
+            legal_move = str(legal_move)
+            move_list.append(legal_move)  # Print each legal move
+
+        for move in move_list:
+            print(str(move), end=", ")
+        print("")
+
+if __name__ == "__main__":
+    game = ChessGame()
+    game.main()
